a6ddfc69cd8508f88e66ec82dfeaac0f31f5419c
Yu Chen <yu.chen@live.ie>
2017-11-03 15:35:21 +0800

    add order wrap

---
+++ src/order_wrap/model.ts
@@ -0,0 +1,24 @@
+import { IModel, Model, Schema } from '@ycs/core/lib/db';
+
+const schema = new Schema(
+  {
+    orders: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: '__store_orders',
+      },
+    ],
+    status: {
+      type: String,
+      enum: ['pending', 'paid', 'cancelled'],
+      default: 'pending',
+    },
+  },
+  { timestamps: {} }
+);
+
+export default Model({
+  name: '__store_order_wrap',
+  auth: true,
+  schema,
+});
