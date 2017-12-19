39f8f396761fb880b32ba6daa8b105415d4f29bc
Yu Chen <yu.chen@live.ie>
2017-12-19 20:23:34 +0800

    fixed permission

--- src/order/controller.ts
+++ src/order/controller.ts
@@ -87,14 +87,12 @@ export default class Controller {
     if (!entity) throw Boom.notFound();
     const action: utils.EAction = ctx.request.fields.action;
     try {
-      if (action.startsWith('customer')) {
-        if (!entity.__auth.equals(ctx.request.auth._id))
-          throw Boom.forbidden('No manager permision');
-      } else {
-        if (!isSub(ctx.request.auth.roles, this.config.roles))
-          throw Boom.forbidden('No manager permision');
-      }
-      await utils.act(entity, action);
+      await utils.act(
+        entity,
+        action,
+        ctx.request.fields.msg,
+        ctx.request.fields.extra
+      );
       response(ctx, 200, entity);
     } catch (e) {
       handleError(ctx, e);
--- src/order_wrap/controller.ts
+++ src/order_wrap/controller.ts
@@ -97,13 +97,6 @@ export default class Controller {
     if (!entity) throw Boom.notFound();
     const action: utils.EAction = ctx.request.fields.action;
     try {
-      if (action.startsWith('customer')) {
-        if (!entity.__auth.equals(ctx.request.auth._id))
-          throw Boom.forbidden('No manager permision');
-      } else {
-        if (!isSub(ctx.request.auth.roles, this.config.roles))
-          throw Boom.forbidden('No manager permision');
-      }
       await utils.act(
         entity,
         action,
