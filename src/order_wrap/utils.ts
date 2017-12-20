58318146448a7e19ea0c61bafe6bb81a3e365e18
Yu Chen <yu.chen@live.ie>
2017-12-20 14:52:25 +0800

    add events

--- src/order_wrap/utils.ts
+++ src/order_wrap/utils.ts
@@ -3,6 +3,11 @@ import { act as subAct } from '../order/utils';
 import Order from '../order/model';
 import { Boom } from '@ycs/core/lib/errors';
 import { ERefund, EStatus } from './model';
+import { EventEmitter } from 'events';
+
+export const event = new EventEmitter();
+event.setMaxListeners(0);
+
 
 export async function act(
   entity: any,
@@ -22,6 +27,7 @@ export async function act(
   }
   switch (action) {
     case EAction.CustomerCreate:
+      event.emit(EAction.CustomerCreate, entity);
       break;
     case EAction.CustomerRefund:
       if ([EStatus.Pending, EStatus.Cancelled].includes(entity.status))
@@ -30,18 +36,21 @@ export async function act(
         throw Boom.badData('The refund is already in process');
       entity.refund = ERefund.Pending;
       await entity.save();
+      event.emit(EAction.CustomerRefund, entity);
       break;
     case EAction.CustomerCancel:
       if (entity.status !== EStatus.Pending)
         throw Boom.badData('Current status is not allowed to cancel');
       entity.status = EStatus.Cancelled;
       await entity.save();
+      event.emit(EAction.CustomerCancel, entity);
       break;
     case EAction.CustomerSign:
       if (entity.status !== EStatus.Unsigned)
         throw Boom.badData('Current status is not allowed to sign');
       entity.status = EStatus.Completed;
       await entity.save();
+      event.emit(EAction.CustomerSign, entity);
       break;
     case EAction.CustomerComment:
       if (entity.status !== EStatus.Completed)
@@ -49,44 +58,52 @@ export async function act(
       // TODO
       entity.status = EStatus.Commented;
       await entity.save();
+      event.emit(EAction.CustomerComment, entity);
       break;
     case EAction.SupplierAccept:
       if (entity.status !== EStatus.Paid)
         throw Boom.badData('Current status is not allowed to accept');
       entity.status = EStatus.Processing;
       await entity.save();
+      event.emit(EAction.SupplierAccept, entity);
       break;
     case EAction.SupplierReject:
       if (entity.status !== EStatus.Paid)
         throw Boom.badData('Current status is not allowed to reject');
       entity.status = EStatus.Cancelled;
       await entity.save();
+      event.emit(EAction.SupplierReject, entity);
       break;
     case EAction.SupplierComplete:
       if (entity.status !== EStatus.Processing)
         throw Boom.badData('Current status is not allowed to complete');
       entity.status = EStatus.Unsigned;
       await entity.save();
+      event.emit(EAction.SupplierComplete, entity);
       break;
     case EAction.SupplierRefundAccept:
       if (entity.refund !== ERefund.Pending)
         throw Boom.badData('Current refund status is not allowed to accept');
       entity.refund = ERefund.Processing;
       await entity.save();
+      event.emit(EAction.SupplierRefundAccept, entity);
       break;
     case EAction.SupplierRefundReject:
       if (entity.refund !== ERefund.Pending)
         throw Boom.badData('Current refund status is not allowed to reject');
       entity.refund = ERefund.Rejected;
       await entity.save();
+      event.emit(EAction.SupplierRefundReject, entity);
       break;
     case EAction.SystemRefundComplete:
       if (entity.refund !== ERefund.Processing)
         throw Boom.badData('Current refund status is not allowed to complete');
       entity.refund = ERefund.Completed;
       await entity.save();
+      event.emit(EAction.SystemRefundComplete, entity);
       break;
     case EAction.SystemError:
+      event.emit(EAction.SystemError, entity);
       break;
     case EAction.SystemPay:
       if (entity.status !== EStatus.Pending)
@@ -94,6 +111,7 @@ export async function act(
       entity.status = EStatus.Paid;
       entity.paidBy = extra.paidBy;
       await entity.save();
+      event.emit(EAction.SystemPay, entity);
       break;
     default:
       throw Boom.badData('Unkown Action:' + action);
