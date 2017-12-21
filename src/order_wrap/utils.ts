3964ce36f69145e97a24f871a281208bc80f6ca6
Yu Chen <yu.chen@live.ie>
2017-12-21 18:35:21 +0800

    emit more info

--- src/order_wrap/utils.ts
+++ src/order_wrap/utils.ts
@@ -26,7 +26,7 @@ export async function act(
   }
   switch (action) {
     case EAction.CustomerCreate:
-      event.emit(EAction.CustomerCreate, entity);
+      event.emit(EAction.CustomerCreate, entity, action, msg, extra);
       break;
     case EAction.CustomerRefund:
       if ([EStatus.Pending, EStatus.Cancelled].includes(entity.status))
@@ -35,21 +35,21 @@ export async function act(
         throw Boom.badData('The refund is already in process');
       entity.refund = ERefund.Pending;
       await entity.save();
-      event.emit(EAction.CustomerRefund, entity);
+      event.emit(EAction.CustomerRefund, entity, action, msg, extra);
       break;
     case EAction.CustomerCancel:
       if (entity.status !== EStatus.Pending)
         throw Boom.badData('Current status is not allowed to cancel');
       entity.status = EStatus.Cancelled;
       await entity.save();
-      event.emit(EAction.CustomerCancel, entity);
+      event.emit(EAction.CustomerCancel, entity, action, msg, extra);
       break;
     case EAction.CustomerSign:
       if (entity.status !== EStatus.Unsigned)
         throw Boom.badData('Current status is not allowed to sign');
       entity.status = EStatus.Completed;
       await entity.save();
-      event.emit(EAction.CustomerSign, entity);
+      event.emit(EAction.CustomerSign, entity, action, msg, extra);
       break;
     case EAction.CustomerComment:
       if (entity.status !== EStatus.Completed)
@@ -57,52 +57,52 @@ export async function act(
       // TODO
       entity.status = EStatus.Commented;
       await entity.save();
-      event.emit(EAction.CustomerComment, entity);
+      event.emit(EAction.CustomerComment, entity, action, msg, extra);
       break;
     case EAction.SupplierAccept:
       if (entity.status !== EStatus.Paid)
         throw Boom.badData('Current status is not allowed to accept');
       entity.status = EStatus.Processing;
       await entity.save();
-      event.emit(EAction.SupplierAccept, entity);
+      event.emit(EAction.SupplierAccept, entity, action, msg, extra);
       break;
     case EAction.SupplierReject:
       if (entity.status !== EStatus.Paid)
         throw Boom.badData('Current status is not allowed to reject');
       entity.status = EStatus.Cancelled;
       await entity.save();
-      event.emit(EAction.SupplierReject, entity);
+      event.emit(EAction.SupplierReject, entity, action, msg, extra);
       break;
     case EAction.SupplierComplete:
       if (entity.status !== EStatus.Processing)
         throw Boom.badData('Current status is not allowed to complete');
       entity.status = EStatus.Unsigned;
       await entity.save();
-      event.emit(EAction.SupplierComplete, entity);
+      event.emit(EAction.SupplierComplete, entity, action, msg, extra);
       break;
     case EAction.SupplierRefundAccept:
       if (entity.refund !== ERefund.Pending)
         throw Boom.badData('Current refund status is not allowed to accept');
       entity.refund = ERefund.Processing;
       await entity.save();
-      event.emit(EAction.SupplierRefundAccept, entity);
+      event.emit(EAction.SupplierRefundAccept, entity, action, msg, extra);
       break;
     case EAction.SupplierRefundReject:
       if (entity.refund !== ERefund.Pending)
         throw Boom.badData('Current refund status is not allowed to reject');
       entity.refund = ERefund.Rejected;
       await entity.save();
-      event.emit(EAction.SupplierRefundReject, entity);
+      event.emit(EAction.SupplierRefundReject, entity, action, msg, extra);
       break;
     case EAction.SystemRefundComplete:
       if (entity.refund !== ERefund.Processing)
         throw Boom.badData('Current refund status is not allowed to complete');
       entity.refund = ERefund.Completed;
       await entity.save();
-      event.emit(EAction.SystemRefundComplete, entity);
+      event.emit(EAction.SystemRefundComplete, entity, action, msg, extra);
       break;
     case EAction.SystemError:
-      event.emit(EAction.SystemError, entity);
+      event.emit(EAction.SystemError, entity, action, msg, extra);
       break;
     case EAction.SystemPay:
       if (entity.status !== EStatus.Pending)
@@ -110,7 +110,7 @@ export async function act(
       entity.status = EStatus.Paid;
       entity.paidBy = extra.paidBy;
       await entity.save();
-      event.emit(EAction.SystemPay, entity);
+      event.emit(EAction.SystemPay, entity, action, msg, extra);
       break;
     default:
       throw Boom.badData('Unkown Action:' + action);
